9c2dae6db86614cf8cae76e0ac055d76ebe89528
Eloy Hernandez <eloyhz@gmail.com>
2021-04-22 19:31:30 -0500

    Day 3 - Control Flow

---
+++ 003_control_flow/day-3-5-exercise/main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+name = (name1 + name2).lower()
+true = name.count('t') + name.count('r') + name.count('u') + name.count('e')
+love = name.count('l') + name.count('o') + name.count('v') + name.count('e')
+score = true * 10 + love
+if score < 10 or score > 90:
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
